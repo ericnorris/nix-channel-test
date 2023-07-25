d50caeb20fb8fb09733ac2598e4ab56baa76d6f1
Eric Norris <enorris@etsy.com>
2023-07-25 10:08:23 -0400

    ci: add aarch64

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
+{ system ? builtins.currentSystem }:
 let
   sources = import ./nix/sources.nix;
-  nixpkgs = import sources.nixpkgs {};
+  nixpkgs = import sources.nixpkgs { inherit system; };
 
   callPackage = nixpkgs.lib.callPackageWith (nixpkgs // pkgs);
 
